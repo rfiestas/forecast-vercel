21cb415d8a2f674e559ad82c42357afd5e9210e0
rfiestas <rfiestas@newrelic.com>
2020-04-22 19:10:59 +0200

    move handlers

--- api/forecast.go
+++ api/forecast.go
@@ -3,11 +3,8 @@ package handler
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"net/url"
-	"os"
-	"strings"
 
 	p "github.com/rfiestas/forecast"
 )
@@ -29,27 +26,5 @@ func ForecastAPIV1(w http.ResponseWriter, r *http.Request) {
 	var location string
 	location = getQueryKey(r, "location", "Barcelona")
 	fmt.Fprint(w, p.GetForecastAPIV1(url.QueryEscape(location)))
-	return
-}
-
-// GetIndex : Return index.html
-func GetIndex(w http.ResponseWriter, r *http.Request) {
-	var location string
-	location = getQueryKey(r, "location", "Barcelona")
-	data, err := ioutil.ReadFile(p.GetIndexAPIV1())
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	w.Header().Set("Cache-Control", "max-age=86400, public")
-	fmt.Fprint(w, strings.Replace(string(data), "#LOCATION#", location, -1))
-	return
-}
-
-// GetRobots : Return robots.txt
-func GetRobots(w http.ResponseWriter, r *http.Request) {
-	var data string
-	data = "User-agent: *\nDisallow: \n"
-	fmt.Fprint(w, data)
 	return
 }
