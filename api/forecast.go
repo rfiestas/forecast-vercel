4f0f4cb6637bd014b124e182a15375d03fa66da8
rfiestas <rfiestas@gmail.com>
2020-04-25 22:38:48 +0200

    Update: dependecies

--- api/forecast.go
+++ api/forecast.go
@@ -2,29 +2,13 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
-	"net/url"
 
 	p "github.com/rfiestas/forecast"
 )
 
-// getQueryKey : take a http request url query key, assign default value when not exist.
-func getQueryKey(r *http.Request, key string, failure string) string {
-	var value string
-	keys, ok := r.URL.Query()[key]
-	if !ok || len(keys[0]) < 1 {
-		value = failure
-	} else {
-		value = string(keys[0])
-	}
-	return url.QueryEscape(value)
-}
-
 // Handler is the ForecastAPIV1, Forecast call
 func Handler(w http.ResponseWriter, r *http.Request) {
-	var location string
-	location = getQueryKey(r, "location", "Barcelona")
-	fmt.Fprint(w, p.GetForecastAPIV1(url.QueryEscape(location)))
+	p.ForecastAPIV1(w, r)
 	return
 }
